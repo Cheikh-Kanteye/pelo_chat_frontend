08adb0ab9f8a5641155b4ecb0fb2f738c501f5de
ababacar-wade <cheikhababacar02@gmail.com>
2026-02-23 12:54:38 +0000

    interface chat

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,17 @@
 module com.example.pelo_chat {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;      // ← AJOUTER
+    requires javafx.graphics;
     requires static lombok;
     requires com.google.gson;
+
 
     opens com.example.pelo_chat to javafx.fxml;
     opens com.example.pelo_chat.view to javafx.fxml;
     opens com.example.pelo_chat.controller to javafx.fxml;
     opens com.example.pelo_chat.utils to com.google.gson;
-    opens com.example.pelo_chat.model to com.google.gson;    // ← AJOUTER
-    opens com.example.pelo_chat.service to javafx.fxml;      // ← AJOUTER
+    opens com.example.pelo_chat.model to com.google.gson;
+    opens com.example.pelo_chat.service to javafx.fxml;
 
     exports com.example.pelo_chat;
     exports com.example.pelo_chat.view;
